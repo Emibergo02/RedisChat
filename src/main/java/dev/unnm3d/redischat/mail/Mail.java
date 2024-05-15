df93a29a82ea0181c6f1367649d671366335aeed
Emibergo02 <36164338+Emibergo02@users.noreply.github.com>
2024-05-15 23:25:48 +0200

    New mail

--- src/main/java/dev/unnm3d/redischat/mail/Mail.java
+++ src/main/java/dev/unnm3d/redischat/mail/Mail.java
@@ -1,13 +1,14 @@
 package dev.unnm3d.redischat.mail;
 
-import dev.unnm3d.redischat.RedisChat;
 import lombok.Getter;
 import lombok.Setter;
-import net.kyori.adventure.inventory.Book;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 import org.jetbrains.annotations.NotNull;
 import xyz.xenondevs.inventoryaccess.component.AdventureComponentWrapper;
 import xyz.xenondevs.invui.item.ItemProvider;
@@ -17,31 +18,51 @@ import xyz.xenondevs.invui.item.impl.AbstractItem;
 import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.TimeZone;
 
 @Getter
 public class Mail extends AbstractItem {
 
-    final double id;
+    private final double id;
+    private final MailGUIManager manager;
     @Setter
     MailCategory category;
-    final String sender;
-    final String receiver;
-    final String title;
+    private final String sender;
+    private final String receiver;
+    private final String title;
     @Setter
-    String content;
+    private boolean read = false;
+    @Setter
+    private String content;
 
-    public Mail(String sender, String receiver, String title) {
+    /**
+     * Creates a new mail
+     *
+     * @param sender   The sender of the mail
+     * @param receiver The mail receiver
+     * @param title    The title of the mail
+     */
+    public Mail(@NotNull MailGUIManager manager, @NotNull String sender, @NotNull String receiver, @NotNull String title) {
         this.id = System.currentTimeMillis() + ((int) (Math.random() * 100.0) / 100.0);
+        this.manager = manager;
         this.category = receiver.equals("-Public") ? MailCategory.PUBLIC : MailCategory.PRIVATE;
         this.sender = sender;
         this.receiver = receiver;
         this.title = title;
     }
 
-    public Mail(double id, String serialized) {
-        String[] splitted = serialized.split("§§");
+    /**
+     * Creates a mail from a serialized string
+     *
+     * @param id         The mail id
+     * @param serialized The serialized mail string
+     */
+    public Mail(@NotNull MailGUIManager manager, double id, String serialized) {
+        this.manager = manager;
+        final String[] splitted = serialized.split("§§");
         this.id = id;
         this.category = MailCategory.fromString(splitted[0]);
         this.sender = splitted[1];
@@ -50,34 +71,138 @@ public class Mail extends AbstractItem {
         this.content = splitted[4];
     }
 
-
     public String serialize() {
         return category.toString() + "§§" + sender + "§§" + receiver + "§§" + title + "§§" + content;
     }
 
+    public String serializeWithId() {
+        return id + "§§" + category.toString() + "§§" + sender + "§§" + receiver + "§§" + title + "§§" + content;
+    }
+
     @Override
     public ItemProvider getItemProvider() {
-        if (content.isEmpty()) return new ItemBuilder(Material.PAPER);
-        ZonedDateTime cal = ZonedDateTime.ofInstant(Instant.ofEpochMilli((long) id), TimeZone.getTimeZone(RedisChat.getInstance().config.mailTimestampZone).toZoneId());
+        ItemBuilder itemBuilder = new ItemBuilder(Material.PAPER);
+        if (content.isEmpty()) {
+            return itemBuilder;
+        }
+        if (read) {
+            itemBuilder = new ItemBuilder(manager.getPlugin().guiSettings.mailItem);
+        } else {
+            itemBuilder = new ItemBuilder(manager.getPlugin().guiSettings.unreadMailItem);
+        }
 
-        String timestampFormat = " <aqua>" + DateTimeFormatter.ofPattern(RedisChat.getInstance().config.mailTimestampFormat).format(cal);
-        return new ItemBuilder(RedisChat.getInstance().guiSettings.mailItem)
-                .setDisplayName(new AdventureComponentWrapper(RedisChat.getInstance().getComponentProvider().parse("<yellow>" + title + timestampFormat)))
-                .addLoreLines(new AdventureComponentWrapper(RedisChat.getInstance().getComponentProvider().parse("<grey>" + content)));
+        final ZonedDateTime cal = ZonedDateTime.ofInstant(Instant.ofEpochMilli((long) id),
+                TimeZone.getTimeZone(manager.getPlugin().config.mailTimestampZone).toZoneId());
+        final String timestampFormat = " <aqua>" + DateTimeFormatter.ofPattern(manager.getPlugin().config.mailTimestampFormat).format(cal);
+
+        return itemBuilder.setDisplayName(new AdventureComponentWrapper(manager.getPlugin().getComponentProvider().parse("<yellow>" + title + timestampFormat)))
+                .addLoreLines(new AdventureComponentWrapper(manager.getPlugin().getComponentProvider().parse("<grey>" + content)));
     }
 
     @Override
     public void handleClick(@NotNull ClickType clickType, @NotNull Player player, @NotNull InventoryClickEvent event) {
-        openPreview(player);
+        if (clickType.isLeftClick()) {
+            openPreview(player, true);
+        } else if (clickType.isRightClick()) {
+            manager.openMailOptionsGui(player, this);
+        }
     }
 
-    public void openPreview(@NotNull Player player) {
+    /**
+     * Opens the mail preview to the player
+     *
+     * @param player     The player to open the preview to
+     * @param readStatus If the mail should be marked as read
+     */
+    public void openPreview(@NotNull Player player, boolean readStatus) {
         player.closeInventory();
         //Craft written book with mail contents
-        RedisChat.getInstance().getComponentProvider().openBook(player, Book.builder()
-                .title(RedisChat.getInstance().getComponentProvider().parse(title))
-                .addPage(RedisChat.getInstance().getComponentProvider().parse(content))
+        final ItemStack writtenBook = new ItemStack(Material.WRITTEN_BOOK);
+
+        List<Component> components = new ArrayList<>();
+        List<String> headerLines = Arrays.asList(manager.getPlugin().messages.mailHeader
+                .replace("%sender%", sender)
+                .replace("%title%", title)
+                .replace("%timestamp%", ZonedDateTime.ofInstant(Instant.ofEpochMilli((long) id),
+                                TimeZone.getTimeZone(manager.getPlugin().config.mailTimestampZone).toZoneId())
+                        .format(DateTimeFormatter.ofPattern(manager.getPlugin().config.mailTimestampFormat))
+                )
+                .split("\r?\n"));
+
+        final String[] contentLines = content.split("\r?\n");
+
+        // Add the header lines to the first page
+        StringBuilder firstPageText = new StringBuilder(String.join("\n", headerLines) + "\n");
+        int lineCount = headerLines.size();
+        // Fill the rest of the first page with content lines
+        int contentIndex = 0;
+        while (lineCount < 14 && contentIndex < contentLines.length) {
+            String line = contentLines[contentIndex++];
+            // Split the line into multiple lines if it is too long
+            manager.getPlugin().getLogger().info(((line.length() / 14) + 1) + " " + (14 - lineCount));
+            if ((line.length() / 14) + 1 <= 14 - lineCount) {
+                while (line.length() > 19) {
+                    firstPageText.append(line, 0, 19).append("\n");
+                    line = line.substring(19);
+                    lineCount++;
+                    manager.getPlugin().getLogger().info("Line substr: " + line);
+                }
+            } else {
+                // Commit to the new page and start a new one
+                contentIndex--;
+                break;
+            }
+            if (!line.isEmpty()) {
+                firstPageText.append(line).append("\n");
+
+                lineCount++;
+            }
+        }
+
+        // Add the first component to the list
+        components.add(manager.getPlugin().getComponentProvider().parse(firstPageText.toString()));
+
+        // Create all the other pages with remaining content
+        while (contentIndex < contentLines.length) {
+            StringBuilder componentText = new StringBuilder(); //The new page
+            lineCount = 0;
+            // Fills the page with content
+            while (lineCount < 14 && contentIndex < contentLines.length) {
+                String line = contentLines[contentIndex++];
+
+                //Check if the line is too long to fit in the page
+                manager.getPlugin().getLogger().info(((line.length() / 14) + 1) + " " + (13 - lineCount));
+                if ((line.length() / 14) + 1 <= 14 - lineCount) {
+                    while (line.length() > 19) {// Split the line into multiple lines if it is too long
+                        componentText.append(line, 0, 19).append("\n<dark_gray>");
+                        line = line.substring(19);
+                        lineCount++;
+                    }
+                } else {
+                    //Commit to the new page and start a new one
+                    contentIndex--;
+
+                    break;
+                }
+                if (!line.isEmpty()) {
+                    componentText.append(line).append("\n<dark_gray>");
+
+                    lineCount++;
+                }
+            }
+            components.add(manager.getPlugin().getComponentProvider().parse(componentText.toString()));
+        }
+
+
+        writtenBook.setItemMeta(((BookMeta) writtenBook.getItemMeta())
+                .toBuilder()
+                .author(manager.getPlugin().getComponentProvider().parse(sender))
+                .title(manager.getPlugin().getComponentProvider().parse(title))
+                .pages(components)
                 .build());
+        player.openBook(writtenBook);
+        System.out.println("Opening mail preview content:" + content);
+        manager.readMail(this, player, readStatus);
     }
 
 
