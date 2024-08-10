4a79b7024ea94bf5803916c487f07d1b29f3207e
Emibergo02 <36164338+Emibergo02@users.noreply.github.com>
2024-08-10 22:13:38 +0200

    Getter for PlayerChannel fields

--- src/main/java/dev/unnm3d/redischat/api/events/ChannelGuiPopulateEvent.java
+++ src/main/java/dev/unnm3d/redischat/api/events/ChannelGuiPopulateEvent.java
@@ -3,20 +3,23 @@ package dev.unnm3d.redischat.api.events;
 import dev.unnm3d.redischat.channels.gui.PlayerChannel;
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-@Setter
 @Getter
 public class ChannelGuiPopulateEvent extends Event {
     private static final HandlerList HANDLERS = new HandlerList();
+    private final Player player;
+    @Setter
     private List<PlayerChannel> channelItems;
 
-    public ChannelGuiPopulateEvent(List<PlayerChannel> channelItems) {
+    public ChannelGuiPopulateEvent(Player player, List<PlayerChannel> channelItems) {
         super(true);
+        this.player = player;
         this.channelItems = channelItems;
     }
 
