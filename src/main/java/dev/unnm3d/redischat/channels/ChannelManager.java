f4900b2eb0ef908b4e9c83ab2d869df771640129
quiquelhappy <quiquelhappy@gmail.com>
2025-10-26 11:16:52 +0100

    update deprecated sound methods

--- src/main/java/dev/unnm3d/redischat/channels/ChannelManager.java
+++ src/main/java/dev/unnm3d/redischat/channels/ChannelManager.java
@@ -345,7 +345,7 @@ public class ChannelManager extends RedisChatAPI {
             if (getComponentProvider().purgeTags(chatMessage.getContent()).contains(recipient.getName())) {
                 if (!plugin.config.mentionSound.isEmpty()) {
                     final String[] split = plugin.config.mentionSound.split(":");
-                    recipient.playSound(recipient.getLocation(), Sound.valueOf(split[0]).getKey().toString(),
+                    recipient.playSound(recipient.getLocation(), Sound.valueOf(split[0]),
                             Float.parseFloat(split[1]), Float.parseFloat(split[2]));
                 }
             }
@@ -354,7 +354,7 @@ public class ChannelManager extends RedisChatAPI {
             if (chatMessage.getReceiver().isPlayer()) {
                 if (!plugin.config.privateMessageSound.isEmpty()) {
                     final String[] split = plugin.config.privateMessageSound.split(":");
-                    recipient.playSound(recipient.getLocation(), Sound.valueOf(split[0]).getKey().toString(),
+                    recipient.playSound(recipient.getLocation(), Sound.valueOf(split[0]),
                             Float.parseFloat(split[1]), Float.parseFloat(split[2]));
                 }
             }
